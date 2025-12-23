26e982280ac091ca6facb478f9c477a74d20b822
Sid-dev19 <shivsid17@gmail.com>
2025-12-23 13:09:27 +0530

    Initial clean commit without secrets

---
+++ routes/pool.js
@@ -0,0 +1,16 @@
+const mysql = require("mysql2");
+
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  connectionLimit: 100,
+  multipleStatements: true,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
+
+module.exports = pool;
