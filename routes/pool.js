d7b090b0aaf2af23bf7ceaaffb50cdd654416223
Sid-dev19 <shivsid17@gmail.com>
2025-12-23 16:32:04 +0530

    fix: add ssl for aiven mysql

--- routes/pool.js
+++ routes/pool.js
@@ -6,14 +6,19 @@ const pool = mysql.createPool({
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   port: process.env.DB_PORT || 3306,
+
+  ssl: {
+    rejectUnauthorized: true,
+  },
+
   waitForConnections: true,
-  connectionLimit: 10,
+  connectionLimit: 20,
   queueLimit: 0
 });
 
 pool.getConnection((err, conn) => {
   if (err) {
-    console.error("❌ DB CONNECTION FAILED:", err.message);
+    console.error("❌ DB CONNECTION FAILED:", err);
   } else {
     console.log("✅ DB CONNECTED SUCCESSFULLY");
     conn.release();
